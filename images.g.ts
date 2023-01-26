15f5704e0d581d7e6a70348ee0fa043ef5d6eb75
bmanzo1 <123478188+bmanzo1@users.noreply.github.com>
2023-01-25 23:49:06 -0600

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- images.g.ts
+++ images.g.ts
@@ -5,11 +5,11 @@ namespace myImages {
         switch(helpers.stringTrim(name)) {
             case "image1":
             case "spiderman":return img`
-. . . . . . . . . . . . . . . . 
-. . . . 2 2 2 2 2 2 2 2 . . . . 
+. . . 2 2 2 2 2 2 2 2 2 2 . . . 
 . . 2 2 2 2 2 2 2 2 2 2 2 2 . . 
-. 2 2 2 2 2 2 2 2 2 2 2 2 2 . . 
-. 2 f f 2 2 2 2 2 2 2 2 f f 2 . 
+. 2 2 2 2 2 2 2 2 2 2 2 2 2 2 . 
+. 2 2 2 2 2 2 2 2 2 2 2 2 2 2 . 
+2 2 f f 2 2 2 2 2 2 2 2 f f 2 2 
 2 f f f f f 2 2 2 2 f f f f f 2 
 2 f f 1 1 f 2 2 2 2 f 1 1 f f 2 
 2 2 f 1 1 1 f 2 2 f 1 1 1 f 2 2 
